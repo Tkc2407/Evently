920e67984e7909263493976be96ba7b62944b9fe
Tkc2407 <tapaschaubey@gmail.com>
2024-07-11 17:36:30 +0530

    added navitems

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -9,23 +9,23 @@ const NavItems = () => {
   const pathname = usePathname();
 
   return (
-    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+    <ul className="md:flex-between md:flex-row flex w-full flex-col items-start gap-5 ">
       {headerLinks.map((link) => {
         const isActive = pathname === link.route;
-        
+
         return (
           <li
             key={link.route}
             className={`${
-              isActive && 'text-primary-500'
+              isActive && "text-primary-500"
             } flex-center p-medium-16 whitespace-nowrap`}
           >
             <Link href={link.route}>{link.label}</Link>
           </li>
-        )
+        );
       })}
     </ul>
-  )
+  );
 }
 
 export default NavItems
