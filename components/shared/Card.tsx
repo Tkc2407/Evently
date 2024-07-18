996e00f31893bad32a6d6fd5e89d67b70d73a207
Tkc2407 <tapaschaubey@gmail.com>
2024-07-18 19:02:31 +0530

    cards of created events done

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -37,12 +37,10 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         </div>
       )}
 
-      <div
-        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
-      > 
+      <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"> 
        {!hidePrice && <div className="flex gap-2">
-          <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-            {event.isFree ? 'FREE' : `$${event.price}`}
+          <span className="p-semibold-14 rounded-full bg-green-100 px-4 py-1 text-green-60">
+            {event.isFree ? 'FREE' : `Rs ${event.price}`}
           </span>
           <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
             {event.category.name}
